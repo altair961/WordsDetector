d5a1536bd8e2ee3678eaa2b1c2b505b18a121754
altair961 <altair89@list.ru>
2019-11-04 20:57:15 +0300

    Added black pixels to an image in

--- words_detector/words_detector/words_detector.cpp
+++ words_detector/words_detector/words_detector.cpp
@@ -4,6 +4,12 @@
 int main(int argc, char** argv) {
 	cv::Mat img = cv::imread(argv[1], -1);
 	if (img.empty()) return -1;
+
+	img.at<uchar>(5, 5) = 0;
+	img.at<uchar>(6, 5) = 0;
+	img.at<uchar>(7, 5) = 0;
+	img.at<uchar>(8, 5) = 0;
+
 	cv::namedWindow("Example1", cv::WINDOW_AUTOSIZE);
 	cv::imshow("Example1", img);
 	cv::waitKey(0);
